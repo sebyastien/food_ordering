98c6cd24943d8405afc5c301ab7f241d2b9a934a
Pape Momar DIOP <pape-momar.diop@student-cs.fr>
2025-12-14 18:29:42 +0100

    Full sync local -> AWS (working local version)

--- admin/connection.php
+++ admin/connection.php
@@ -1,6 +1,5 @@
 <?php
-$link=mysqli_connect("localhost","root","") or die (mysqli_error($link));
-mysqli_select_db($link,"food_order") or die (mysqli_error($link));
-// AJOUTER CETTE LIGNE
-mysqli_set_charset($link, "utf8mb4"); 
+$link = mysqli_connect("localhost","fooduser","foodpass123","food_order");
+if(!$link){ die(mysqli_connect_error()); }
 ?>
+
